ea672330a32140708439a1292647c76672a990a5
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-28 19:28:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,9 +10,9 @@ class ComputeAVGMaxMin
     float Average;
    float Maximum;
    float Minimum;
-    float CalculateAvg(float report[]);
-    float CalculateMax(float report[]);
-    float CalculateMin(float report[]);
+    float CalculateAvg(const std::vector<float>& report);
+    float CalculateMax(const std::vector<float>& report);
+    float CalculateMin(const std::vector<float>& report);
 };
    ComputeAVGMaxMin ComputeStatistics(const std::vector<float>& report);
 }
