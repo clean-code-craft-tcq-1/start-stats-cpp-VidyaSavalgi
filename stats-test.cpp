4f4e1a6071baa68b61c9993db77205ad2f5da6f8
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-29 00:20:01 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -34,7 +34,8 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     const float maxThreshold = 10.2;
    // StatsAlerter statsAlerter(maxThreshold, alerters);
     statsAlerterobj.checkAndAlert({99.8, 34.2, 4.5, 6.7});
-
+cout<<emailAlert.emailSent<<endl;
+    cout<<ledAlert.ledGlows<<endl;
     REQUIRE(emailAlert.emailSent);
     REQUIRE(ledAlert.ledGlows);
 }
