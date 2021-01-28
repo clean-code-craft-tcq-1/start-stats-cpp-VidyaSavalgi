f334567cc97453a906c5f9684d972201ebcc070c
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-28 17:41:17 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,9 +4,9 @@
 #include "stats.h"
 
 #include <cmath>
-
+Statistics obj;
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    auto computedStats = obj.ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.Average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.Maximum - 8.9) < epsilon);
