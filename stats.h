2d11bd1e36513fc047dad2918071fda7b084c5a0
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-28 15:04:23 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,8 +1,8 @@
 #include <vector>
 
 namespace Statistics {
-    void ComputeStatistics(const std::vector<float> report);
-    float CalculateAvg(const std::vector<float> report);
-    float CalculateMax(const std::vector<float> report);
-    float CalculateMin(const std::vector<float> report);
+    void ComputeStatistics(const std::vector<float>& report);
+    float CalculateAvg(const std::vector<float>& report);
+    float CalculateMax(const std::vector<float>& report);
+    float CalculateMin(const std::vector<float>& report);
 }
