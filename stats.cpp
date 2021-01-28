f0244d3c3fa2b14cb0139da2347ac776664446f7
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-28 19:57:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -21,7 +21,8 @@ double ComputeAVGMaxMin::CalculateAvg(const std::vector<double>& report)
 {
     double sum = 0;
     double Avg = 0;
-    for(int i=0;i<report.size();i++)
+    std::vector<double>& *iter;
+    for(int i = iter.begin();i<iter.end();i++)
     {
         sum = sum + report[i];
     }
