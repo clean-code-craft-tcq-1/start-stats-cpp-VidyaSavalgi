9f416206b6d4f62bd98e1c8b7db120f67cd2e84b
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-29 01:32:31 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -72,10 +72,10 @@ double ComputeAVGMaxMin::CalculateMin(const std::vector<double>& report)
    
     return Min;
 }
-void statsAlerter::checkAndAlert(const std::vector<double>& alertvalues)
+void StatsAlerter ::checkAndAlert(const std::vector<double>& alertvalues)
 {
     ComputeAVGMaxMin statreport;
-   statsAlerter checkstatsAlerter;
+   StatsAlerter checkstatsAlerter;
     
     EmailAlert checkEmailAlert;
     LEDAlert checkLEDAlert;
