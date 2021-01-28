da034bccb40fdd7ece6f94a1d790478ca380ace6
VidyaSavalgi <77484079+VidyaSavalgi@users.noreply.github.com>
2021-01-28 17:13:25 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,44 +5,36 @@ int main()
 {
 
     
-void Statistics::ComputeStatistics(const std::vector<float>& report)
+void ComputeStatistics(float report[])
 {
     //Implement statistics here
     
-    int size =0;
     float Average = 0;
     float Maximum = 0;
     float Minimum = 0;
-    /*cout<<"enter the number of elements"<<endl;
-    cin>>size;
-    for(int i=0;i<size;i++)
-    {
-        cout<<"enter the number"<<endl;
-        cin>>report[i];
-    }
-    report.push_back(rstatarray)*/
-
+    
+    size = sizeof(report)/sizeof(report[0])
     Average = CalculateAvg(report);
     Maximum = CalculateMax(report);
     Minimum = CalculateMin(report);
 } 
-float Statistics::CalculateAvg(const std::vector<float>& report)
+float CalculateAvg(float report[])
 {
     float sum = 0;
     float Avg = 0;
-    for(int i=0;i<report.size;i++)
+    for(int i=0;i<size;i++)
     {
         sum = sum + report[i];
     }
-    Avg = sum/report.size;
+    Avg = sum/size;
 
     return Avg;
 }
 
-float Statistics::CalculateMax(const std::vector<float>& report)
+float CalculateMax(float report[])
 {
     float Max = statinput[0];
-    for(int i=1;i<report.size;i++)
+    for(int i=1;i<size;i++)
     {
        if(Max > report[i])
        {
@@ -52,10 +44,10 @@ float Statistics::CalculateMax(const std::vector<float>& report)
     return Max;
 }
 
-float Statistics::CalculateMin(const std::vector<float>& report)
+float CalculateMin(float report[])
 {
      float Min = statinput[0];
-    for(int i=1;i<report.size;i++)
+    for(int i=1;i<size;i++)
     {
        if(Min < report[i])
        {
@@ -64,6 +56,4 @@ float Statistics::CalculateMin(const std::vector<float>& report)
     }
     return Min;
 }
-
-
 }
